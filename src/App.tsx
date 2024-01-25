a4ff59a99f35a1a9b3972f1207e96abc409df3d5
Sara Thampi <saraannthampi@gmail.com>
2024-01-25 16:30:45 +0000

    fetch added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './assets/Header'
+import { useEffect } from 'react'
+
+const formatDate = date =>
+  `${date.getHours()}:${String(date.getMinutes()).padStart(2, '0')} ${String(
+    date.getSeconds(),
+  ).padStart(2, '0')}.${String(date.getMilliseconds()).padStart(3, '0')}`
+
+async function fetchPokemon(name) {
+  const pokemonQuery = `
+    query PokemonInfo($name: String) {
+      pokemon(name: $name) {
+        id
+        number
+        name
+        attacks {
+          special {
+            name
+            type
+            damage
+          }
+        }
+      }
+    }
+  `
+
+  const response = await window.fetch('https://graphql-pokemon2.vercel.app/', {
+    // learn more about this API here: https://graphql-pokemon2.vercel.app/
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+    body: JSON.stringify({
+      query: pokemonQuery,
+      variables: {name: name.toLowerCase()},
+    }),
+  })
+
+  const {data, errors} = await response.json()
+  if (response.ok) {
+    const pokemon = data?.pokemon
+    if (pokemon) {
+      // add fetchedAt helper (used in the UI to help differentiate requests)
+      console.log(pokemon)
+      pokemon.fetchedAt = formatDate(new Date())
+      return pokemon
+    } else {
+      return Promise.reject(new Error(`No pokemon with the name "${name}"`))
+    }
+  } else {
+    // handle the graphql errors
+    const error = new Error(errors?.map(e => e.message).join('\n') ?? 'unknown')
+    return Promise.reject(error)
+  }
+}
+
+
+
+async function fetchData(){
+  const response = await fetch("https://api.solar.sheffield.ac.uk/pvlive/api/v4/pes/10?start=2022-07-01T00:00:00&end=2022-07-02T00:00:00" , {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+        },
+  })
+  console.log(response)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   const data = async () => {
+  //   fetchData()
+  //   console.log(data)
+  //   }
+  // }, []
 
+  // )
+fetchPokemon("pikachu")
+fetchData()
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Header /> 
     </>
   )
 }
---
+++ src/assets/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return(
+    <h1>Solar Energy</h1>
+    )
+}
