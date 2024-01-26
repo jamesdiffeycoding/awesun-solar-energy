dbc123584359f591fde7f584e712a2b0552c6116
Sara Thampi <saraannthampi@gmail.com>
2024-01-26 17:01:01 +0000

    background

--- my-project/src/components/SolarData.jsx
+++ my-project/src/components/SolarData.jsx
@@ -3,9 +3,11 @@
 export default function SolarData(){
     const randomNum = Math.floor(Math.random() * 6);
     return(
-        <>
-            <h1>awesun sunbers</h1> 
-            <p>{randomNum}GW</p>
-        </>
+        
+            <div>
+                <h1 className="text-slate-950">awesun sunbers</h1> 
+                <p>{randomNum}GW</p>
+            </div>
+    
     )
 }
