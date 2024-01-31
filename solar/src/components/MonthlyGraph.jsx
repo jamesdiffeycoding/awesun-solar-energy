89c74e9478a18f5a735a99e9507f71b80d2cac68
Sara Thampi <saraannthampi@gmail.com>
2024-01-31 22:52:56 +0000

    keys added to all graphs

--- solar/src/components/MonthlyGraph.jsx
+++ solar/src/components/MonthlyGraph.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from "react"
+import React from "react"
 
 export default function MonthlyGraph({dayTimeSolarDataMonth, dayTimeSolarDataBarWidthMonth, highestSolarDataValueMonth}) {
     
@@ -10,21 +11,21 @@ export default function MonthlyGraph({dayTimeSolarDataMonth, dayTimeSolarDataBar
                 <div className="graph">
                     
                     {dayTimeSolarDataMonth.reverse().map((dataPoint, index) => (dataPoint[1].includes("09:00:00")) ? (
-                    <>
+                    <React.Fragment key={`${index}-frag`}>
                     <div className="verticalstrip" key={index}>
-                    <div className="break"></div>
+                    <div className="break" key={`${index}-break`}></div>
                         </div>
-                    <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthMonth}%` }}>
-                        <div className="bar" style={{ height:  `${98*dataPoint[2]/highestSolarDataValueMonth}%`  }}></div>
+                    <div className="verticalstrip" key={`${index}-strip`} style={{ width: `${dayTimeSolarDataBarWidthMonth}%` }}>
+                        <div className="bar" key={`${index}-bar`} style={{ height:  `${98*dataPoint[2]/highestSolarDataValueMonth}%`  }}></div>
                     </div>
-                    </>
+                    </React.Fragment>
                     ) : (
-                    <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthMonth}%` }}>
-                        <div className="bar" style={{ height:  `${98*dataPoint[2]/highestSolarDataValueMonth}%`  }}></div>
+                    <div className="verticalstrip" key={`${index}-2`} style={{ width: `${dayTimeSolarDataBarWidthMonth}%` }}>
+                        <div className="bar" key={`${index}-bar-2`} style={{ height:  `${98*dataPoint[2]/highestSolarDataValueMonth}%`  }}></div>
                     </div>
                     ))}
                 </div>
-                <div className="graph-base"></div>
+                <div className="graph-base" key="base"></div>
 
             </div>
         </div>
--- solar/src/components/YearlyGraph.jsx
+++ solar/src/components/YearlyGraph.jsx
@@ -1,26 +1,27 @@
 "use client"
 import { useState } from "react"
+import React from "react";
 
 export default function YearlyGraph({dayTimeSolarDataYear, dayTimeSolarDataBarWidthYear, highestSolarDataValueYear}){
   return (
     <div>
       {/* YEAR CONTAINER  */}
       <div className="graph-container">
-        <div className="graph">
+        <div className="graph" key="graph2">
           {dayTimeSolarDataYear.reverse().map((dataPoint, index) => (
             (
-              <>
+              <React.Fragment key={`${index}-frag`}>
                 <div className="verticalstrip" key={index}>
-                  {dataPoint[1].includes("01T") && <div className="break2"></div>}
+                  {dataPoint[1].includes("01T") && <div className="break2" key={`${index}-break`}></div>}
                 </div>
-                <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthYear}%` }}>
-                  <div className="bar" style={{ height: `${98 * dataPoint[2] / highestSolarDataValueYear}%` }}></div>
+                <div className="verticalstrip" key={`${index}-2`} style={{ width: `${dayTimeSolarDataBarWidthYear}%` }}>
+                  <div className="bar" key={`${index}-bar-2`} style={{ height: `${98 * dataPoint[2] / highestSolarDataValueYear}%` }}></div>
                 </div>
-              </>
+              </React.Fragment>
             ) 
           ))}
         </div>
-        <div className="graph-base"></div>
+        <div className="graph-base" key="base"></div>
       </div>
     </div>
   );
