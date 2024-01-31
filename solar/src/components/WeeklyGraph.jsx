8cbe5eeae6a93e172071c3fab55bca01368ae935
Sara Thampi <saraannthampi@gmail.com>
2024-01-31 22:41:28 +0000

    unique key props

--- solar/src/components/WeeklyGraph.jsx
+++ solar/src/components/WeeklyGraph.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from "react"
+import React from "react"
 
 export default function WeeklyGraph({dayTimeSolarDataWeek, dayTimeSolarDataBarWidthWeek, highestSolarDataValueWeek}){
     
@@ -8,24 +9,24 @@ export default function WeeklyGraph({dayTimeSolarDataWeek, dayTimeSolarDataBarWi
         {/* WEEK CONTAINER */}
     
             <div className="graph-container">
-            <div className="graph">
+            <div className="graph" key="graph">
                 {dayTimeSolarDataWeek.reverse().map((dataPoint, index) => (dataPoint[1].includes("09:00:00")) ? (
-                <>
+                <React.Fragment key={`${index}-frag`} >
                 <div className="verticalstrip" key={index}>
-                    <div className="break"></div>
+                    <div className="break" key={`${index}-break`} ></div>
                 </div>
-                <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthWeek}%` }}>
-                    <div className="bar" style={{ height:  `${98*dataPoint[2]/highestSolarDataValueWeek}%`  }}></div>
+                <div className="verticalstrip" key={`${index}-strip`} style={{ width: `${dayTimeSolarDataBarWidthWeek}%` }}>
+                    <div className="bar" style={{ height:  `${98*dataPoint[2]/highestSolarDataValueWeek}%`  }} key={`${index}-bar`} ></div>
                 </div>
-                </>
+                </React.Fragment>
                 ) : (
-                <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthWeek}%` }}>
+                <div className="verticalstrip" key={`${index}-2`} style={{ width: `${dayTimeSolarDataBarWidthWeek}%` }}>
                     {/* <p className="popup-data" style={{  position: 'absolute',  top: '50%', left: `${100*index/dayTimeSolarDataWeek.length}%`, color: 'green', backgroundColor: 'red'}}>1</p> */}
-                    <div className="bar" style={{ height: `${98*dataPoint[2]/highestSolarDataValueWeek}%` }}></div>
+                    <div className="bar" style={{ height: `${98*dataPoint[2]/highestSolarDataValueWeek}%` }} key={`${index}-bar2`}></div>
                 </div>
                 ))}
             </div>
-                <div className="graph-base"></div>
+                <div className="graph-base" key="base" ></div>
             </div>
         </div>        
     )
