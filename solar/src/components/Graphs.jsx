e7e824313de85e80204539dc378088d032d454e1
mattkirke <matthewkirke@icloud.com>
2024-01-31 16:20:06 +0000

    graph styling

--- solar/src/components/Graphs.jsx
+++ solar/src/components/Graphs.jsx
@@ -1,63 +1,81 @@
-"use client"
+"use client";
 import WeeklyGraph from "./WeeklyGraph.jsx";
 import MonthlyGraph from "./MonthlyGraph.jsx";
 import YearlyGraph from "./YearlyGraph.jsx";
 import { useState, useEffect } from "react";
 
-
-
 // STYLES
-import "../App.css"
-
-
+import "../App.css";
 
 export default function Graphs({
-  dayTimeSolarDataWeek,
-  dayTimeSolarDataBarWidthWeek,
-  highestSolarDataValueWeek,
-  dayTimeSolarDataMonth,
-  dayTimeSolarDataBarWidthMonth,
-  highestSolarDataValueMonth,
-  dayTimeSolarDataYear,
-  dayTimeSolarDataBarWidthYear,
-  highestSolarDataValueYear,
+	dayTimeSolarDataWeek,
+	dayTimeSolarDataBarWidthWeek,
+	highestSolarDataValueWeek,
+	dayTimeSolarDataMonth,
+	dayTimeSolarDataBarWidthMonth,
+	highestSolarDataValueMonth,
+	dayTimeSolarDataYear,
+	dayTimeSolarDataBarWidthYear,
+	highestSolarDataValueYear,
 }) {
-  const [display, setDisplay] = useState("last week");
+	const [display, setDisplay] = useState("last week");
 
-  const handleDisplay = (event) => {
-    setDisplay(event.target.textContent);
-  };
+	const handleDisplay = (event) => {
+		setDisplay(event.target.textContent);
+	};
 
-
-  return (
-    <>
-      <div>Choose a date range: </div>
-      <div>
-        <span onClick={handleDisplay} className={`cursor-pointer hover:text-slate-900 ${display==='last week' ? 'underline' : 'no-underline'}`}>last week</span> /{' '}
-        <span onClick={handleDisplay} className={`cursor-pointer hover:text-slate-900 ${display==='last month' ? 'underline' : 'no-underline'}`} >last month</span> /{' '}
-        <span onClick={handleDisplay} className={`cursor-pointer hover:text-slate-900 ${display==='last year' ? 'underline' : 'no-underline'}`} >last year</span>
-      </div>
-      {display === "last week" && (
-        <WeeklyGraph
-          dayTimeSolarDataWeek={dayTimeSolarDataWeek}
-          dayTimeSolarDataBarWidthWeek={dayTimeSolarDataBarWidthWeek}
-          highestSolarDataValueWeek={highestSolarDataValueWeek}
-        />
-      )}
-      {display === "last month" && (
-        <MonthlyGraph
-          dayTimeSolarDataMonth={dayTimeSolarDataMonth}
-          dayTimeSolarDataBarWidthMonth={dayTimeSolarDataBarWidthMonth}
-          highestSolarDataValueMonth={highestSolarDataValueMonth}
-        />
-      )}
-      {display === "last year" && (
-        <YearlyGraph
-          dayTimeSolarDataYear={dayTimeSolarDataYear}
-          dayTimeSolarDataBarWidthYear={dayTimeSolarDataBarWidthYear}
-          highestSolarDataValueYear={highestSolarDataValueYear}
-        />
-      )}
-    </>
-  );
+	return (
+		<>
+			<div className="graph-selector">
+				<div>Choose a date range: </div>
+				<span
+					onClick={handleDisplay}
+					className={`cursor-pointer hover:text-slate-900 ${
+						display === "last week" ? "underline" : "no-underline"
+					}`}
+				>
+					last week
+				</span>{" "}
+				/{" "}
+				<span
+					onClick={handleDisplay}
+					className={`cursor-pointer hover:text-slate-900 ${
+						display === "last month" ? "underline" : "no-underline"
+					}`}
+				>
+					last month
+				</span>{" "}
+				/{" "}
+				<span
+					onClick={handleDisplay}
+					className={`cursor-pointer hover:text-slate-900 ${
+						display === "last year" ? "underline" : "no-underline"
+					}`}
+				>
+					last year
+				</span>
+			</div>
+			{display === "last week" && (
+				<WeeklyGraph
+					dayTimeSolarDataWeek={dayTimeSolarDataWeek}
+					dayTimeSolarDataBarWidthWeek={dayTimeSolarDataBarWidthWeek}
+					highestSolarDataValueWeek={highestSolarDataValueWeek}
+				/>
+			)}
+			{display === "last month" && (
+				<MonthlyGraph
+					dayTimeSolarDataMonth={dayTimeSolarDataMonth}
+					dayTimeSolarDataBarWidthMonth={dayTimeSolarDataBarWidthMonth}
+					highestSolarDataValueMonth={highestSolarDataValueMonth}
+				/>
+			)}
+			{display === "last year" && (
+				<YearlyGraph
+					dayTimeSolarDataYear={dayTimeSolarDataYear}
+					dayTimeSolarDataBarWidthYear={dayTimeSolarDataBarWidthYear}
+					highestSolarDataValueYear={highestSolarDataValueYear}
+				/>
+			)}
+		</>
+	);
 }
