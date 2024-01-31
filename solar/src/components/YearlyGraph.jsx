06c48160effa3d9f463f5427a08681d0736ce9d3
Sara Thampi <saraannthampi@gmail.com>
2024-01-31 15:16:56 +0000

    yearly graph shows once a day at 2pm

--- solar/src/components/YearlyGraph.jsx
+++ solar/src/components/YearlyGraph.jsx
@@ -2,30 +2,27 @@
 import { useState } from "react"
 
 export default function YearlyGraph({dayTimeSolarDataYear, dayTimeSolarDataBarWidthYear, highestSolarDataValueYear}){
-  return(
-        <div>
-            {/* YEAR CONTAINER  */}  
-        <div className="graph-container">
-          <div className="graph">
-            {dayTimeSolarDataYear.reverse().map((dataPoint, index) => (dataPoint[1].includes("09:00:00") && dataPoint[1].includes("01T")) ? (
+  return (
+    <div>
+      {/* YEAR CONTAINER  */}
+      <div className="graph-container">
+        <div className="graph">
+          {dayTimeSolarDataYear.reverse().map((dataPoint, index) => (
+            (
               <>
-              <div className="verticalstrip" key={index}>
-              <div className="break2"></div>
-              </div>
-              <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthYear}%` }}>
-              <div className="bar" style={{ height:  `${98*dataPoint[2]/highestSolarDataValueYear}%`  }}></div>
-              </div>
+                <div className="verticalstrip" key={index}>
+                  {dataPoint[1].includes("01T") && <div className="break2"></div>}
+                </div>
+                <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthYear}%` }}>
+                  <div className="bar" style={{ height: `${98 * dataPoint[2] / highestSolarDataValueYear}%` }}></div>
+                </div>
               </>
-            ) : (
-              <div className="verticalstrip" key={index} style={{ width: `${dayTimeSolarDataBarWidthYear}%` }}>
-                <div className="bar" style={{ height:  `${98*dataPoint[2]/highestSolarDataValueYear}%`  }}></div>
-              </div>
-            ))}
-          </div>
-          <div className="graph-base"></div>
-
+            ) 
+          ))}
         </div>
-
-        </div>
-    )
+        <div className="graph-base"></div>
+      </div>
+    </div>
+  );
+  
 }
