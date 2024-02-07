cad73248a13bbef80af43734fe95b3bd2a3e1d4b
James <jamesdiffey8@gmail.com>
2024-02-07 14:52:42 +0000

    add favicon

--- solar/src/app/layout.js
+++ solar/src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Awesun Energy Visualiser",
+  title: "Awesun Solar Visualiser",
   description: "Visualising UK solar energy power production",
 };
 
