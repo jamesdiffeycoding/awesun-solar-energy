c4a8949e7922ac945c397568b3340e32e8ed89d5
James Diffey <diffeyj@outlook.com>
2024-08-21 06:53:13 +0100

    text update

--- solar/src/components/SolarTitle.jsx
+++ solar/src/components/SolarTitle.jsx
@@ -1,13 +1,13 @@
 export default function SolarTitle() {
-	return (
-        <div>
-            <h1 className="font-bold text-3xl">AWESUN</h1>
-            <p>Visualising the UK's solar energy</p>
-            <p className="italic text-slate-900 hide-when-portrait">Data graciously provided by the University of Sheffield.</p>
-            <div className="text-xs">
-                    <div className="pt-1">1 MW is enough to power <span className="text-yellow-500">10,000</span> homes. </div>
+        return (
+                <div>
+                        <h1 className="font-bold text-3xl">AWESUN</h1>
+                        <p>Visualising the UK's solar energy</p>
+                        <p className="italic text-slate-900 hide-when-portrait">Data provided by the University of Sheffield.</p>
+                        <div className="text-xs">
+                                <div className="pt-1">1 MW is enough to power <span className="text-yellow-500">10,000</span> homes. </div>
+                        </div>
                 </div>
-        </div>
-        
-	);
+
+        );
 }
